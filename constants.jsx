ffcaebf34305770a78684bea5ed0871154ae00d2
Mykola Khomiakov <vkontemail4@gmail.com>
2024-08-26 16:38:11 +0300

    Do constants

---
+++ constants.jsx
@@ -0,0 +1,16 @@
+import "./src/images";
+
+const constants = {
+  Person1: {
+    portret: "./src/images/John.png",
+    name: "John Doe",
+    description: "I am web developer. I love to design new user-interfaces!",
+  },
+  Person2: {
+    portret: "./src/images/Linda.png",
+    name: "Linda Doe",
+    description: "I am a product manager. I love to make a projects!",
+  },
+};
+
+export default constants;
